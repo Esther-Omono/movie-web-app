44e9f87bf2de4a4373d149bf0da4e42bde8d8faa
Esther-Omono <enitanatakere@gmail.com>
2023-09-10 23:40:07 +0100

    Added the footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Footer = () => {
+    const data = [
+        {
+            icon: '',
+            name: '',
+        },
+        {
+            icon: '',
+            name: '',
+        },
+        {
+            icon: '',
+            name: '',
+        },
+    ];
+
+    return (
+        <>
+            <div>
+                <h2>Footer</h2>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
